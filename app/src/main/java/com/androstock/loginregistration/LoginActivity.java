94174892490ee44a82d413cbfbd5ab6a681d1be1
Ony <onykhan95@gmail.com>
2019-12-02 01:26:01 +0600

    intent from login to bus [for demo purpose]

--- app/src/main/java/com/androstock/loginregistration/LoginActivity.java
+++ app/src/main/java/com/androstock/loginregistration/LoginActivity.java
@@ -4,11 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +23,19 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        login = findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenBusActivity();
+            }
+        });
+
+
+    }
+    public void OpenBusActivity(){
+        Intent intent = new Intent(this,BusActivity.class);
+        startActivity(intent);
     }
 }
